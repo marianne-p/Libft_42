06f7676c0ead6d0597afbcb1f8fabcdd57bd9d21
Mariana <mariana1pihur@gmail.com>
2024-01-15 12:38:00 +0000

    atoi fix

--- ft_atoi.c
+++ ft_atoi.c
@@ -17,22 +17,17 @@ int	ft_atoi(const char *nptr)
 	int	i;
 	int	res;
 	int	neg;
-	int	pos;
 
 	neg = 1;
 	i = 0;
 	res = 0;
 	while (nptr[i] == ' ' || (nptr[i] > 8 && nptr[i] < 14))
 		i++;
-	pos = i;
-	while (!(nptr[i] >= '0' && nptr[i] <= '9'))
+	if (nptr[i] == '-')
 	{
-		if (nptr[i] == '-' && neg != -1)
-			neg = neg * -1;
+		neg = neg * -1;
 		i++;
 	}
-	if (!(pos + 1 == i && (nptr[pos] == '-' || nptr[pos] == '+')) || pos != i)
-		return (0);
 	while (nptr[i] != '\0' && (nptr[i] >= '0' && nptr[i] <= '9'))
 	{
 		res = res * 10 + nptr[i] - '0';
