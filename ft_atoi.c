fe731a13d8a7a5b4491aff036b75f5bda2d70865
Mariana Pihur <mpihur@c3r4s3.42london.com>
2024-01-29 16:46:17 +0000

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,31 +6,32 @@
 /*   By: mpihur <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 20:40:43 by mpihur            #+#    #+#             */
-/*   Updated: 2023/10/31 20:45:28 by mpihur           ###   ########.fr       */
+/*   Updated: 2024/01/29 16:42:49 by mpihur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *nptr)
+long long	ft_atoi(const char *nptr)
 {
-	int	i;
-	int	res;
-	int	neg;
+	long long	i;
+	long long	res;
+	int		neg;
 
 	neg = 1;
 	i = 0;
 	res = 0;
 	while (nptr[i] == ' ' || (nptr[i] > 8 && nptr[i] < 14))
 		i++;
-	if (nptr[i] == '-')
+	if (nptr[i] == '-' || nptr[i] == '+')
 	{
-		neg = neg * -1;
+		if (nptr[i] == '-')
+			neg = neg * -1;
 		i++;
 	}
 	while (nptr[i] != '\0' && (nptr[i] >= '0' && nptr[i] <= '9'))
 	{
-		res = res * 10 + nptr[i] - '0';
+		res = (res * 10) + (nptr[i] - '0');
 		i++;
 	}
 	return (res * neg);
