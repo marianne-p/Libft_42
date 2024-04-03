429d62bcd39b0455d625818fa453b7e17bee011f
Mariana Pihur <mpihur@c1r1s6.42london.com>
2024-04-03 15:15:37 +0100

    Norm fixed, lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: mpihur <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 13:45:10 by mpihur            #+#    #+#             */
-/*   Updated: 2023/11/09 15:07:14 by mpihur           ###   ########.fr       */
+/*   Updated: 2024/04/03 15:15:10 by mpihur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	while (*lst)
 	{
 		last = (*lst)->next;
-		ft_lstdelone(*lst, del);
+		free((*lst)->content);
+		free(*lst);
 		*lst = last;
 	}
+	*lst = NULL;
 }
